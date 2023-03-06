85eeea5b786d4a7f4621b5c03aa1b2fa06d379f3
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-03-06 13:19:25 +0100

    fix: change to looser versions constraints as per best practices
    See documentation [here](https://developer.hashicorp.com/terraform/language/providers/requirements#version-constraints).

--- terraform.tf
+++ terraform.tf
@@ -2,35 +2,35 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = ">= 3"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = ">= 2"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0"
+      version = ">= 2"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.0"
+      version = ">= 2"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0"
+      version = ">= 3"
     }
     utils = {
       source  = "cloudposse/utils"
-      version = "~> 1.0"
+      version = ">= 1"
     }
     argocd = {
       source  = "oboukili/argocd"
-      version = "~> 4.0"
+      version = ">= 4"
     }
   }
 
