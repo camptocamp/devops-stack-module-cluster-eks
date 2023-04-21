3245fa2c906464b0910afefe59cf685abacd0b5a
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2023-04-21 09:54:15 +0200

    docs: fix styling / typos
    Co-authored-by: Gonçalo Heleno <33546359+lentidas@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "base_domain" {
   description = <<-EOT
     The base domain for the cluster.
 
-    This module needs a Route53 zone matching this variable with permission to create DNS records. It will create a wildcard CNAME record `*.apps.<base_domain>` that points to an Elastic Load Balancer routing ingress traffic to all cluster nodes. Such urls will be used by default by other DevOps Stack modules for the applications they deploy (e.g. Argo CD, Prometheus, etc).
+    This module needs a Route 53 zone matching this variable with permission to create DNS records. It will create a wildcard CNAME record `*.apps.<base_domain>` that points to an Elastic Load Balancer routing ingress traffic to all cluster nodes. Such urls will be used by default by other DevOps Stack modules for the applications they deploy (e.g. Argo CD, Prometheus, etc).
   EOT
   type        = string
   default     = null
