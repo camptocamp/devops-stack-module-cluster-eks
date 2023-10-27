094c6649c32de158bfbedfeaf66e0ce8c72b14d2
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2023-10-27 11:02:51 +0200

    chore: terraform fmt

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  nlb_ip = var.create_public_nlb ? data.dns_a_record_set.nlb[0].addrs[0] : ( var.create_private_nlb ? data.dns_a_record_set.nlb_private[0].addrs[0]  : "null" )
+  nlb_ip = var.create_public_nlb ? data.dns_a_record_set.nlb[0].addrs[0] : (var.create_private_nlb ? data.dns_a_record_set.nlb_private[0].addrs[0] : "null")
 
   base_domain = coalesce(var.base_domain, format("%s.nip.io", replace(local.nlb_ip, ".", "-")))
 }
