2cd903b4fd707a02f8a5698e78acb40aacbe6216
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2024-02-21 09:44:26 +0100

    fix: make subdomain variable non-nullable

--- variables.tf
+++ variables.tf
@@ -17,6 +17,7 @@ variable "subdomain" {
   description = "The subdomain used for ingresses."
   type        = string
   default     = "apps"
+  nullable = false
 }
 
 variable "kubernetes_version" {
